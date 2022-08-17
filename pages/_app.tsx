abba1aea867dd3d531167ab199d5614f1f1ff624
null <wasimthoufiq21@gmail.com>
2022-08-17 11:20:38 +0530

    Use created Provider in app.ts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,70 +1,33 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-import { MantineProvider } from '@mantine/core';
-import Head from 'next/head';
-import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
-import {
-  ConnectionProvider,
-  WalletProvider
-} from "@solana/wallet-adapter-react";
-import {
-  LedgerWalletAdapter,
-  PhantomWalletAdapter,
-  SlopeWalletAdapter,
-  SolflareWalletAdapter,
-  SolletExtensionWalletAdapter,
-  SolletWalletAdapter,
-  TorusWalletAdapter
-} from "@solana/wallet-adapter-wallets";
-import React, { FC, useMemo } from "react";
-import { AutoConnectProvider } from 'components/AutoConnect';
-
-export const DEFAULT_ENDPOINT = "https://api.devnet.solana.com"
-  
-// Default styles that can be overridden by your app
-require("@solana/wallet-adapter-react-ui/styles.css");
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { MantineProvider } from "@mantine/core";
+import Head from "next/head";
+import { SolanaProviders } from "components/Providers";
 
 function MyApp({ Component, pageProps }: AppProps) {
-
-  const wallets = useMemo(
-    () => [
-      new PhantomWalletAdapter(),
-      new SlopeWalletAdapter(),
-      new SolflareWalletAdapter(),
-      new TorusWalletAdapter(),
-      new LedgerWalletAdapter(),
-      new SolletWalletAdapter({}),
-      new SolletExtensionWalletAdapter({}),
-    ],
-    []
-  );
-
   return (
     <>
       <Head>
         <title>Mobius</title>
-        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
       </Head>
-      <AutoConnectProvider>
-      <ConnectionProvider endpoint={DEFAULT_ENDPOINT}>
-      <WalletProvider wallets={wallets} autoConnect>
-      <WalletModalProvider>
-      <MantineProvider
-        withGlobalStyles
-        withNormalizeCSS
-        theme={{
-          /** Put your mantine theme override here */
-          colorScheme: 'light',
-        }}
-      >
-        <Component {...pageProps} />
-      </MantineProvider>
-      </WalletModalProvider>
-      </WalletProvider>
-      </ConnectionProvider>
-      </AutoConnectProvider>
+      <SolanaProviders>
+        <MantineProvider
+          withGlobalStyles
+          withNormalizeCSS
+          theme={{
+            /** Put your mantine theme override here */
+            colorScheme: "light",
+          }}
+        >
+          <Component {...pageProps} />
+        </MantineProvider>
+      </SolanaProviders>
     </>
   );
 }
 
-export default MyApp
+export default MyApp;
