e7a2bf06ef026783252d0b668197d5d59b145355
0xSavant <joey.meere@gmail.com>
2022-08-16 22:46:53 -0400

    fix StepTwo

--- components/CreateFundraiser/StepTwo.tsx
+++ components/CreateFundraiser/StepTwo.tsx
@@ -3,6 +3,10 @@ import * as Yup from "yup";
 import React from "react";
 import FormField from "./FormField";
 import { MobiusClient } from "utils/mobius.client";
+import { MOBIUS_IDL } from "utils/const";
+import { MOBIUS_PROG_ID } from "utils/const";
+import { Connection, PublicKey, clusterApiUrl } from '@solana/web3.js';
+import { Program, Provider, web3 } from '@project-serum/anchor';
 
 const validationSchema = Yup.object().shape({
   name: Yup.string().required().label("Name"),
@@ -11,6 +15,14 @@ const validationSchema = Yup.object().shape({
   description: Yup.string().required().label("Description"),
   denomination: Yup.string().required().label("Denomination"),
 });
+
+const { SystemProgram, Keypair } = web3;
+const programID = new PublicKey(MOBIUS_IDL.metadata.address);
+const network = clusterApiUrl('devnet');
+const opts = {
+  preflightCommitment: "confirmed"
+}
+const donator = Keypair.generate();
 
 function StepTwo() {
   const denominations = ["SOL"]; //Add in USDC later
@@ -24,7 +36,41 @@ function StepTwo() {
         description: "",
       }}
       onSubmit={(values, { setSubmitting }) => {
-        // Create fundraiser codes here...
+       /*
+        async function createFundraiser(name, description, image_link) {
+         
+          try {
+        
+            window.Buffer = Buffer;
+            const provider = getProvider();
+            const program = new Program(MOBIUS_IDL, MOBIUS_PROG_ID, provider);
+            const { fundraiserConfig, bump } = await getProgramDerivedCampainWritingAccountAddress();
+        
+            await program.rpc.initialize(new BN(bump), {
+              accounts: {
+                fundraiserConfig: fundraiserConfig,
+                authority: provider.wallet.publicKey,
+                systemProgram: SystemProgram.programId,
+              },
+            });
+        
+            await program.rpc.createFundraiser(name, description, image_link, {
+              accounts: {
+                fundraiserConfig: fundraiserConfig,
+                authority: provider.wallet.publicKey,
+        
+              },
+            });
+        
+            await getAllFundraisers();
+            let account = await program.account.fundraiser.fetch(fundraiserAccount);
+            console.log("Added a new Campaign with Details");
+        
+          } catch (error) {
+            console.log("Error Creating Campaign :", error)
+          }
+        }
+        */
       }}
       validationSchema={validationSchema}
     >
